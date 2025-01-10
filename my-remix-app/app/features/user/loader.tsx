a4c7fc05632acd4a4c9dc906e6fbe7ab262f6e42
dongcheol.park <dongcheol.park@privatenote.co.kr>
2025-01-10 18:22:19 +0900

    a

--- my-remix-app/app/features/user/loader.tsx
+++ my-remix-app/app/features/user/loader.tsx
@@ -1,12 +1,12 @@
-import { json } from "@remix-run/cloudflare";
+import { data } from "@remix-run/cloudflare";
 import createLoader from "~/utils/createLoader";
 
 export const indexLoader = createLoader(async ({ db }) => {
   const users = await db.user.findMany();
 
-  return json({ users: users });
+  return data({ users });
 });
 
 export const showLoader = createLoader(async () => {
-  return json({ users: ["DEF"] });
+  return data({ users: ["DEF"] });
 });
