05970ffdbec32f67b6e2464eba1202a48afb3abf
dongcheol.park <dongcheol.park@privatenote.co.kr>
2025-01-13 15:05:45 +0900

    a

--- my-remix-app/app/features/post/action.tsx
+++ my-remix-app/app/features/post/action.tsx
@@ -1,22 +1,21 @@
 import createAction from "~/utils/createAction";
 import { object, string } from "zod";
-import { redirect } from "@remix-run/cloudflare";
+import { json, redirect } from "@remix-run/cloudflare";
 
 const postSchema = object({
-  title: string(),
+  title: string().max(20, "제목은 20자를 넘을 수 없습니다."),
   content: string(),
 });
 
 export const insertAction = createAction(async ({ db, request }) => {
   const formdata = await request.formData();
-  const data = await postSchema
-    .parseAsync({
-      title: formdata.get("title"),
-      content: formdata.get("content"),
-    })
-    .catch((error) => {
-      throw new Error(error.errors[0].message);
-    });
+  const { data, error } = postSchema.safeParse({
+    title: formdata.get("title"),
+    content: formdata.get("content"),
+  });
+  if (error) {
+    return json({ error: error.issues });
+  }
 
   await db.post.create({ data });
   return redirect("/posts");
--- my-remix-app/app/features/post/pages/create.tsx
+++ my-remix-app/app/features/post/pages/create.tsx
@@ -1,14 +1,19 @@
+import { Form, useActionData } from "@remix-run/react";
 import styles from "./create.module.scss";
+import { insertAction } from "~/features/post/action";
 
 export default function Create() {
+  const actionData = useActionData<typeof insertAction>();
+
   return (
     <div className={styles.root}>
       <h1>Post Create</h1>
-      <form method="post" className={styles.formContainer}>
+      {actionData && <p>{actionData.error[0].message}</p>}
+      <Form method="post" className={styles.formContainer}>
         <input type="text" name="title" placeholder="Title" />
         <textarea name="content" placeholder="Content"></textarea>
         <button type="submit">Submit</button>
-      </form>
+      </Form>
     </div>
   );
 }
