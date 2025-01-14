9867fb6e43fec6e3c56dfe440091cbfc601ea00d
dongcheol.park <dongcheol.park@privatenote.co.kr>
2025-01-14 11:31:13 +0900

    add edit page

--- my-remix-app/app/routes/posts.$postId.edit.tsx
+++ my-remix-app/app/routes/posts.$postId.edit.tsx
@@ -1,10 +1,13 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
 import { updateAction } from "~/features/post/action";
+import { updateLoader } from "~/features/post/loader";
 import Edit from "~/features/post/pages/edit";
 
 export const meta: MetaFunction = () => {
   return [{ title: "Post Edit" }];
 };
+
+export const loader = updateLoader;
 
 export const action = updateAction;
 
